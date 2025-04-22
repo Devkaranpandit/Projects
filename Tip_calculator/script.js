187a5c84cecbd4b629b2d7de07eb8c5b108aea36
Devkaranpandit <devkaranxb@gmail.com>
2025-04-22 21:26:45 +0530

    Update script.js

--- Tip_calculator/script.js
+++ Tip_calculator/script.js
@@ -4,7 +4,11 @@ const total = document.getElementById('total');
 const calc = document.getElementById('calc');
 
 function calculate(){
-    const price = amount.value * (1+tip.value / 100)
+    if(amount.value == ''){
+        error.innerHTML = 'Enter a valid amount'
+    }else{
+        const price = amount.value * (1+tip.value / 100)
     total.textContent = price.toFixed(2); 
+    }
 }
 calc.addEventListener('click',calculate);
